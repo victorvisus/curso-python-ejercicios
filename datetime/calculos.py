41b8e83e4d535b7f14e74ba6b1ceb8464e8e197a
victorvisus <victor.vxg@gmail.com>
2026-04-07 13:33:52 +0200

    manejo de fechas

--- datetime/calculos.py
+++ datetime/calculos.py
@@ -9,7 +9,9 @@ print("Tiempo hasta año nuevo: ", time_left_for_newyear)
 
 t1 = datetime(year=2019, month=12, day=5, hour=0, minute=59, second=0)
 t2 = datetime(year=2020, month=1, day=1, hour=0, minute=0, second=0)
+t3 = datetime.now()
 diff = t2 - t1
+diff = t3 - t1
 print("Tiempo hasta año nuevo:", diff)  # Tiempo hasta año nuevo: 26 days, 23:01:00
 
 # Calcular diferencias con timedelta
@@ -17,3 +19,41 @@ t1 = timedelta(weeks=12, days=10, hours=4, seconds=20)
 t2 = timedelta(days=7, hours=5, minutes=3, seconds=30)
 t3 = t1 - t2
 print("t3 =", t3)
+
+# cuanto falta a que acabe el curso
+findeCurso = datetime(year=2026, month=6, day=19, hour=14, minute=30, second=0)
+hoy = datetime.now()
+print("Dias restantes para el curso:", findeCurso - hoy)
+# cuantas semanas es
+
+print("Semanas restantes para el curso:", (findeCurso - hoy) / timedelta(weeks=1))
+"""
+Cómo funciona: Aquí estás dividiendo un objeto timedelta (la duración total) entre otro objeto timedelta (una semana exacta).
+Ventaja: Es muy legible. Si mañana quieres saber los meses (aunque timedelta no tiene meses por su duración variable), o las horas, solo cambias el divisor: / timedelta(hours=1).
+Resultado: Te dará un número decimal (float) muy preciso, incluyendo los días y horas sobrantes como parte decimal.
+"""
+print("Semanas restantes para el curso:", (findeCurso - hoy).days / 7)
+"""
+
+"""
+print("Test --------------------------------------------------")
+diffTiempo = findeCurso - hoy
+print(diffTiempo)
+semanas = diffTiempo // timedelta(weeks=1)
+semanasResto = diffTiempo % timedelta(weeks=1)
+print("Teimpo: ", semanas, " semanas y ", semanasResto)
+
+print("Test - UNIX ---------------------------------------------")
+unixTiempoDiasDif = diffTiempo.total_seconds()
+print("Tiempo en dias: ", unixTiempoDiasDif)
+delta = timedelta(seconds=unixTiempoDiasDif)
+print(delta)
+semanas = delta.days // 7
+dias_restantes = delta.days % 7
+print("Teimpo: ", semanas, " semanas y ", dias_restantes, " dias")
+horas = delta.seconds // 3600
+minutos = (delta.seconds % 3600) // 60
+segundos = (delta.seconds % 3600) % 60
+print(
+    f"Tiempo: {semanas} semanas y {dias_restantes} dias y {horas} horas y {minutos} minutos y {segundos} segundos"
+)
--- variables/11_ejercicios_fechas.py
+++ variables/11_ejercicios_fechas.py
@@ -1,10 +1,10 @@
 """
-Obtén el día, mes, año, hora, minuto y timestamp actuales desde el módulo datetime.
-Formatea la fecha actual con el formato: "%m/%d/%Y, %H:%M:%S"
-Hoy es 5 de diciembre de 2019. Convierte esa cadena de fecha a un objeto datetime.
-Calcula la diferencia entre ahora y el próximo año nuevo.
-Calcula la diferencia entre el 1 de enero de 1970 y ahora.
-Piensa: ¿para qué puedes usar el módulo datetime? Por ejemplo:
+1 Obtén el día, mes, año, hora, minuto y timestamp actuales desde el módulo datetime.
+2 Formatea la fecha actual con el formato: "%m/%d/%Y, %H:%M:%S"
+3 Hoy es 5 de diciembre de 2019. Convierte esa cadena de fecha a un objeto datetime.
+4 Calcula la diferencia entre ahora y el próximo año nuevo.
+5 Calcula la diferencia entre el 1 de enero de 1970 y ahora.
+6 Piensa: ¿para qué puedes usar el módulo datetime? Por ejemplo:
     Análisis de series temporales
     Obtener timestamps para eventos en una aplicación
     Añadir la fecha de publicación en un blog
