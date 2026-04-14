84b35bf7986798b9dc53f27636ac0b4f819b95d2
victorvisus <victor.vxg@gmail.com>
2026-04-14 11:52:52 +0200

    mongoDB

---
+++ 00_ejercicios/26_ejercicios_pythonweb.py
@@ -0,0 +1,17 @@
+"""
+1 Crea una aplicación Flask llamada "Calculadora de calificaciones". El usuario ingresa la nota y el nombre de la asignatura, y la aplicación debe mostrar un mensaje según la nota:
+    Si nota ≥ 90: "¡Excelente! Tu calificación en [Asignatura] es [Nota]".
+    Si 80 ≤ nota < 90: "¡Muy bien! Tu calificación en [Asignatura] es [Nota]".
+    Si 70 ≤ nota < 80: "Regular. Tu calificación en [Asignatura] es [Nota]".
+    Si 60 ≤ nota < 70: "Aprobaste. Tu calificación en [Asignatura] es [Nota]".
+    Si nota < 60: "¡Necesitas esforzarte más! Tu calificación en [Asignatura] es [Nota]".
+
+2 Crea una aplicación "Calculadora de IMC" que calcule el índice de masa corporal (IMC = peso(kg) / altura(m)²) y muestre el estado según el IMC:
+    IMC < 18.5: "Bajo peso"
+    18.5 ≤ IMC < 24.9: "Peso saludable"
+    25 ≤ IMC < 29.9: "Sobrepeso"
+    IMC ≥ 30: "Obesidad"
+
+3 Crea una aplicación de blog donde los usuarios puedan añadir, editar y eliminar publicaciones.
+4 Crea una aplicación "Gestor de tareas" donde los usuarios puedan añadir, ver y eliminar tareas.
+"""
---
+++ python_y_mongoDB/python_y_mongoDB.py
@@ -0,0 +1,23 @@
+"""
+https://github.com/Asabeneh/30-Days-Of-Python/blob/master/Spanish/27_python_with_mongodb_sp.md
+
+MongoDB es una base de datos NoSQL. MongoDB almacena datos en documentos tipo JSON, lo que hace a MongoDB muy flexible y escalable. Veamos la terminología que difiere entre bases de datos SQL y NoSQL. La siguiente tabla mostrará la diferencia entre SQL y NoSQL.
+
+SQL
+    Base de datos
+    Tabla
+    Campo
+    Registro
+    Clave primaria
+
+NoSQL
+    Base de datos
+    Colección
+    Documento
+    Campo
+    Clave primaria
+    Indice
+
+ngvdKu8AKbtMDuUA
+victorvxg_db_user
+"""
