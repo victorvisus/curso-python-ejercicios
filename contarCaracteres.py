1d8ff2537fe84eb67004590be327ccc08fe04231
Cypher <victor.vxg@gmail.com>
2026-05-25 18:07:27 +0200

    ejercicios py

---
+++ contarCaracteres.py
@@ -0,0 +1,3 @@
+cadena = "Jeny & Víctor En el umbral entre la luz y la oscuridad, cuando el año comienza a inclinarse hacia su mitad más sombría, nuestras almas sellarán su destino Te invitamos a formar parte de este pacto 31 oct, 12h Finca Torre Pilar Zaragoza Que la luz del mediodía sea testigo"
+
+print(len(cadena))
